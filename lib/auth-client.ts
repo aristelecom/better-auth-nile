da3272aa2480ebfedb5e3d30e765f187ef12d02c
Ricardo Lujan <riickyluja21@gmail.com>
2024-11-23 20:32:53 -0300

    initial commit

---
+++ lib/auth-client.ts
@@ -0,0 +1,46 @@
+import { createAuthClient } from "better-auth/react";
+import {
+	organizationClient,
+	passkeyClient,
+	twoFactorClient,
+	adminClient,
+	multiSessionClient,
+	oneTapClient,
+} from "better-auth/client/plugins";
+import { toast } from "sonner";
+import { customSessionClient } from "./auth/plugins/session-client";
+
+export const client = createAuthClient({
+	plugins: [
+		organizationClient(),
+		twoFactorClient({
+			onTwoFactorRedirect() {
+				window.location.href = "/two-factor";
+			},
+		}),
+		passkeyClient(),
+		adminClient(),
+		multiSessionClient(),
+		oneTapClient({
+			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+		}),
+		customSessionClient(),
+	],
+	fetchOptions: {
+		onError(e) {
+			if (e.error.status === 429) {
+				toast.error("Too many requests. Please try again later.");
+			}
+		},
+	},
+});
+
+export const {
+	signUp,
+	signIn,
+	signOut,
+	useSession,
+	organization,
+	useListOrganizations,
+	useActiveOrganization,
+} = client;
